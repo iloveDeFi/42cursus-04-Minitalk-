1c0719d8be9a3577db859851c6595e2fcd234221
Baptiste Bessard <bessardbusiness@gmail.com>
2023-04-13 11:11:49 +0200

    client & server

--- client.c
+++ client.c
@@ -6,26 +6,90 @@
 /*   By: bbessard <bbessard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 15:28:35 by bbessard          #+#    #+#             */
-/*   Updated: 2023/03/07 14:05:44 by bbessard         ###   ########.fr       */
+/*   Updated: 2023/04/13 11:08:11 by bbessard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 /*
-** le programme client doit être capable d'envoyer des messages au serveur. 
-** Il doit donc convertir les messages en signaux et les envoyer au serveur.
+** La fonction send_signals prend deux arguments: un PID et un message. 
+** La fonction envoie le message caractère par caractère en convertissant 
+** chaque caractère en sa représentation binaire, puis en envoyant un signal 
+** à un autre processus en fonction de la valeur de chaque bit binaire. 
+** Si le bit est 0, le programme envoie un signal SIGUSR1 et si le bit est 1, 
+** il envoie un signal SIGUSR2. La fonction utilise également la fonction usleep 
+** pour ralentir l'envoi des signaux afin de permettre au processus récepteur 
+** de traiter les signaux reçus.
 */
 
-void	send_signals(int pid, char	*message)
+void send_signals(int pid, char	*message)
 {
-	encrypt message bits
-	send message server PID
-	stop condition for server
+	int				letter;
+	int				i;
+
+	letter = 0;
+	while (message[letter])
+	{
+		i = -1;
+		while (++i < 8)
+		{
+			if (((unsigned char)(message[letter] >> (7 - i)) & 1) == 0)
+				kill(pid, SIGUSR1);
+			else if (((unsigned char)(message[letter] >> (7 - i)) & 1) == 1)
+				kill(pid, SIGUSR2);
+			usleep(50);
+		}
+	letter++;
+	}
+	i = 0;
+	while (i++ < 8)
+	{
+		kill(pid, SIGUSR1);
+		usleep(50);
+	}
 }
 
-int	main(int	sc, char	**av)
+/*
+** La fonction main prend deux arguments: le PID du processus de réception et le 
+** message à envoyer. Le programme vérifie d'abord que deux arguments ont été 
+** passés en entrée, puis il convertit le PID en entier et stocke le message dans 
+** une variable. Si le PID est invalide ou si le message est vide, le programme 
+** affiche un message d'erreur correspondant. Sinon, le programme appelle la fonction 
+** send_signals pour envoyer le message au processus récepteur.
+*/ 
+
+int	main(int argc, char **argv)
 {
+	char				*message;
+	int					server_id;
 
-
+	if (argc == 3)
+	{
+		server_id = ft_atoi(argv[1]);	
+		if (!server_id)
+		{
+			ft_printf("[ERROR]. Wrong arg");
+			return (0);
+		}
+		message = argv[2];
+		if (message[0] == 0)
+		{
+			ft_printf("Tu n'as envoyé aucun texte ! Ecris un truc bro");
+			return (0);
+		}
+		send_signals(server_id, message);
+	}
+	else
+	{
+		ft_printf("[ERROR]. Too much or too few arguments.\n Make sure ");
+		ft_printf("you enter arguments as follow: ./client <PID> <MESSAGE>");
+	}
+	return (0);
 }
+
+/*
+** Le programme envoie des signaux au processus récepteur en utilisant les valeurs 
+** binaires de chaque caractère du message pour communiquer le message caractère 
+** par caractère.
+*/
--- server.c
+++ server.c
@@ -6,29 +6,85 @@
 /*   By: bbessard <bbessard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 15:28:40 by bbessard          #+#    #+#             */
-/*   Updated: 2023/03/07 14:08:52 by bbessard         ###   ########.fr       */
+/*   Updated: 2023/04/13 11:08:08 by bbessard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-/* 
-** Le programme serveur doit être capable de recevoir les messages envoyés par 
-** le client. Il doit donc être capable de recevoir les signaux et de 
-** les convertir en messages compréhensibles."
-*/
- 
- int	main()
- {
-	 Disply PID
-	 while (1)
-	 {
-		 receive message
-	 }
+int	ft_recursive_power(int nb, int power)
+{
+	int	res;
 
-return (0);
+	if (power == 0)
+		return (1);
+	else if (power < 0)
+		return (0);
+	else
+	{
+		res = nb * ft_recursive_power(nb, power - 1);
+		return (res);
+	}
+}
 
-	 Decrypt message
-	 Action after signal received
- }
- 
+char	*letter_to_string(char const *s1, char const letter)
+{
+	int		i;
+	int		j;
+	char	*tab;
+
+	i = 0;
+	j = 0;
+	tab = malloc((ft_strlen(s1) + 2) * sizeof(char));
+	if (!tab)
+		return (NULL);
+	while (s1[i])
+		tab[j++] = s1[i++];
+	i = 0;
+	tab[j++] = letter;
+	tab[j] = 0;
+	free ((void *)(s1));
+	return (tab);
+}
+
+void	signal_handler(int signum)
+{
+	static int	counter = 0;
+	static int	result = 0;
+	static int	len = 0;
+	static char	*final;
+
+	if (!final)
+		final = ft_strdup("");
+	if (signum == SIGUSR1)
+		result = result + 0;
+	else if (signum == SIGUSR2)
+		result = result + (1 * ft_recursive_power(2, 7 - counter));
+	counter++;
+	if (counter == 8)
+	{
+		final = letter_to_string(final, result);
+		if (result == '\0')
+		{
+			ft_printf("%s\n", final);
+			final = NULL;
+		}
+		counter = 0;
+		result = 0;
+		len += 1;
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	signal_received;
+
+	ft_printf("Welcome to my server\n");
+	ft_printf("Server's PID: %d\n", getpid());
+	signal_received.sa_handler = signal_handler;
+	signal_received.sa_flags = 0;
+	sigaction(SIGUSR1, &signal_received, NULL);
+	sigaction(SIGUSR2, &signal_received, NULL);
+	while (1)
+		usleep(50);
+}
